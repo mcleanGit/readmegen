18544890afc8ec8f4fd4e1e462def3f65c6c674e
Don McLean <mclean@utoronto.ca>
2022-02-23 12:41:29 -0500

    begins data mapping

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -47,15 +47,29 @@ renderLicenseLink ()
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-//function renderLicenseSection(license) {
+function renderLicenseSection(license) {
+  writeToFile 
 
-}
-
-/* TODO: Create a function to generate markdown for README
-/function generateMarkdown(data) {
-  return `# ${data.title}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `
+    # ${data.title},
+  ## ${data.description},
+  ## ${data.licenseBadge},
+  ---
+  ## ${data.contents},
+  ---
+  ## ${data.installation},
+  ## ${data.usage},
+  ## ${data.licenseLink},
+  ## ${data.contributing},
+  ## ${data.tests},
+  ---
+  ## ${data.questions},
+  ## ${data.github}
 
 `;
+
 }
-*/
+
 module.exports = generateMarkdown;
