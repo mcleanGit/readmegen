3fea387d815f7353f82d5ea0a891ebdc7f060065
Don McLean <mclean@utoronto.ca>
2022-02-27 00:50:45 -0500

    update in revision

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,45 +1,46 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  if (license === '') {
-    return '';
-  } else if (license === "Apache") {
+function renderLicenseBadge(value) {
+  if (value === "Apache") {
     return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]";
-  } else if (license === "Boost") {
+  } else if (value === "Boost") {
     return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)]";
-  } else if (license === "BSD_3-clause") {
+  } else if (value === "BSD_3-clause") {
     return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]";
-  } else if (license == "Creative Commons") {
+  } else if (value == "Creative Commons") {
     return "[![License: CC0-1.0](https://img.shields.io/badge/License-CC0_1.0-lightgrey.svg)]";
-  } else if (license === "GNU_GPLv3") {
+  } else if (value === "GNU_GPLv3") {
     return "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
-  } else if (license === "MIT") {
+  } else if (value === "MIT") {
     return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
-  } else if (license === "Mozilla") {
+  } else if (value === "Mozilla") {
     return "[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)]";
+  } else {
+      " ";
   }
 }
+
 renderLicenseBadge()
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === '') {
-    return '';
-  } else if (license === "Apache") {
+function renderLicenseLink(value) {
+  if (value === "Apache") {
     return "(https://opensource.org/licenses/Apache-2.0)";
-  } else if (license === "Boost") {
+  } else if (value === "Boost") {
     return "(https://www.boost.org/LICENSE_1_0.txt)";
-  } else if (license === "BSD_3-clause") {
+  } else if (value === "BSD_3-clause") {
     return "(https://opensource.org/licenses/BSD-3-Clause)";
-  } else if (license == "Creative Commons") {
+  } else if (value == "Creative Commons") {
     return "(http://creativecommons.org/publicdomain/zero/1.0/)";
-  } else if (license === "GNU_GPLv3") {
+  } else if (value === "GNU_GPLv3") {
     return "(https://www.gnu.org/licenses/gpl-3.0)";
-  } else if (license === "MIT") {
+  } else if (value === "MIT") {
     return "(https://opensource.org/licenses/MIT)";
-  } else if (license === "Mozilla") {
+  } else if (value === "Mozilla") {
     return "(https://opensource.org/licenses/MPL-2.0)";
+  } else {
+     " ";
   }
 }
 
@@ -58,9 +59,7 @@ module.exports = {
   licenseBadge: this.licenseBadge,
   licenseLink: this.licenseLink,
 }
- */
-// TODO: Create a function to generate markdown for README
-/*
+*/
 function generateMarkdown(data) {
   return `
 
@@ -77,15 +76,13 @@ function generateMarkdown(data) {
   ## ${data.tests},
   ---
   ## ${data.questions},
-  ## ${data.userName},
-  ### ${data.githubLink},
+  ### ${data.userName},
+  ### ${data.github},
   ### ${data.email},
-
-//`;
+  `;
 }
 
-generateMarkdown()
+// generateMarkdown()
 
-module.exports = { generateMarkdown:generateMarkdown, 
-};
-*/
+module.exports = generateMarkdown;
+
